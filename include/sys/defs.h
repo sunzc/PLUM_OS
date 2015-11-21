f4ef7af3bcd59075cf4cc2107b28252ca4b31fd6
Zhichuang Sun <sunzc522@gmail.com>
2015-11-21 01:15:13 -0500

    modify sys/defs.h to adapt to elf.h taste
    Signed-off-by: Zhichuang Sun <sunzc522@gmail.com>

--- include/sys/defs.h
+++ include/sys/defs.h
@@ -12,6 +12,13 @@ typedef          short  int16_t;
 typedef unsigned char	uint8_t;
 typedef          char	 int8_t;
 
+typedef int16_t		__s16;
+typedef uint16_t	__u16;
+typedef int32_t		__s32;
+typedef uint32_t	__u32;
+typedef int64_t		__s64;
+typedef uint64_t	__u64;
+
 typedef uint64_t size_t;
 typedef uint32_t pid_t;
 typedef uint64_t off_t;
