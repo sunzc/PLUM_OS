91cdba260cf84910a51ca3a0f6893f6742f4a38a
Zhichuang Sun <sunzc522@gmail.com>
2015-11-24 03:11:44 -0500

    add exception handlers
    Signed-off-by: Zhichuang Sun <sunzc522@gmail.com>

--- sys/idt.c
+++ sys/idt.c
@@ -2,6 +2,44 @@
 #include <sys/idt.h>
 #include <sys/mm.h>
 
+#define exception_handler_func(n)	\
+	void exception_handler_##n (void) { printf("exception :%d",n); }
+
+#define exception_handler(n)	\
+	exception_handler_##n
+
+exception_handler_func(0)
+exception_handler_func(1)
+exception_handler_func(2)
+exception_handler_func(3)
+exception_handler_func(4)
+exception_handler_func(5)
+exception_handler_func(6)
+exception_handler_func(7)
+exception_handler_func(8)
+exception_handler_func(9)
+exception_handler_func(10)
+exception_handler_func(11)
+exception_handler_func(12)
+exception_handler_func(13)
+exception_handler_func(14)
+exception_handler_func(15)
+exception_handler_func(16)
+exception_handler_func(17)
+exception_handler_func(18)
+exception_handler_func(19)
+exception_handler_func(20)
+exception_handler_func(21)
+exception_handler_func(22)
+exception_handler_func(23)
+exception_handler_func(24)
+exception_handler_func(25)
+exception_handler_func(26)
+exception_handler_func(27)
+exception_handler_func(28)
+exception_handler_func(29)
+exception_handler_func(30)
+exception_handler_func(31)
 /* record the time since boot */
 uint64_t ticks = 0;
 
@@ -19,6 +57,38 @@ void init_idt() {
 	_idtp.base = (uint64_t)&idt_entries;
 
 	memset(&idt_entries, 0, sizeof(idt_entry_t) * 256);
+	idt_set_gate(0, (uint64_t)exception_handler(0), 0x08, 0x8E);
+	idt_set_gate(1, (uint64_t)exception_handler(1), 0x08, 0x8E);
+	idt_set_gate(2, (uint64_t)exception_handler(2), 0x08, 0x8E);
+	idt_set_gate(3, (uint64_t)exception_handler(3), 0x08, 0x8E);
+	idt_set_gate(4, (uint64_t)exception_handler(4), 0x08, 0x8E);
+	idt_set_gate(5, (uint64_t)exception_handler(5), 0x08, 0x8E);
+	idt_set_gate(6, (uint64_t)exception_handler(6), 0x08, 0x8E);
+	idt_set_gate(7, (uint64_t)exception_handler(7), 0x08, 0x8E);
+	idt_set_gate(8, (uint64_t)exception_handler(8), 0x08, 0x8E);
+	idt_set_gate(9, (uint64_t)exception_handler(9), 0x08, 0x8E);
+	idt_set_gate(10, (uint64_t)exception_handler(10), 0x08, 0x8E);
+	idt_set_gate(11, (uint64_t)exception_handler(11), 0x08, 0x8E);
+	idt_set_gate(12, (uint64_t)exception_handler(12), 0x08, 0x8E);
+	idt_set_gate(13, (uint64_t)exception_handler(13), 0x08, 0x8E);
+	idt_set_gate(14, (uint64_t)exception_handler(14), 0x08, 0x8E);
+	idt_set_gate(15, (uint64_t)exception_handler(15), 0x08, 0x8E);
+	idt_set_gate(16, (uint64_t)exception_handler(16), 0x08, 0x8E);
+	idt_set_gate(17, (uint64_t)exception_handler(17), 0x08, 0x8E);
+	idt_set_gate(18, (uint64_t)exception_handler(18), 0x08, 0x8E);
+	idt_set_gate(19, (uint64_t)exception_handler(19), 0x08, 0x8E);
+	idt_set_gate(20, (uint64_t)exception_handler(20), 0x08, 0x8E);
+	idt_set_gate(21, (uint64_t)exception_handler(21), 0x08, 0x8E);
+	idt_set_gate(22, (uint64_t)exception_handler(22), 0x08, 0x8E);
+	idt_set_gate(23, (uint64_t)exception_handler(23), 0x08, 0x8E);
+	idt_set_gate(24, (uint64_t)exception_handler(24), 0x08, 0x8E);
+	idt_set_gate(25, (uint64_t)exception_handler(25), 0x08, 0x8E);
+	idt_set_gate(26, (uint64_t)exception_handler(26), 0x08, 0x8E);
+	idt_set_gate(27, (uint64_t)exception_handler(27), 0x08, 0x8E);
+	idt_set_gate(28, (uint64_t)exception_handler(28), 0x08, 0x8E);
+	idt_set_gate(29, (uint64_t)exception_handler(29), 0x08, 0x8E);
+	idt_set_gate(30, (uint64_t)exception_handler(30), 0x08, 0x8E);
+	idt_set_gate(31, (uint64_t)exception_handler(31), 0x08, 0x8E);
 	idt_set_gate(32, (uint64_t)_isr_wrapper_timer, 0x08, 0x8E);
 	idt_set_gate(33, (uint64_t)_isr_wrapper_kbd, 0x08, 0x8E);
 
